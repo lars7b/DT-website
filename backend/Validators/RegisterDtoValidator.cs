4ae403ab62f18a7ea0dc3510d0c9fa94f5d0bf6e
Lars <larszevenbergen97@gmail.com>
2026-04-09 11:51:39 +0200

    Update RegisterDtoValidator.cs

--- backend/Validators/RegisterDtoValidator.cs
+++ backend/Validators/RegisterDtoValidator.cs
@@ -8,15 +8,15 @@ public class RegisterDtoValidator : AbstractValidator<RegisterDto>
     public RegisterDtoValidator()
     {
         RuleFor(x => x.Email)
-            .NotEmpty().WithMessage("Email is required.")
-            .EmailAddress().WithMessage("Invalid email format.");
+            .NotEmpty().WithMessage("E-mailadres is verplicht.")
+            .EmailAddress().WithMessage("Ongeldig e-mailadres.");
 
         RuleFor(x => x.Password)
-            .NotEmpty().WithMessage("Password is required.")
-            .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
-            .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
-            .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter.")
-            .Matches(@"[0-9]").WithMessage("Password must contain at least one digit.")
-            .Matches(@"[^\w]").WithMessage("Password must contain at least one special character.");
+            .NotEmpty().WithMessage("Wachtwoord is verplicht.")
+            .MinimumLength(8).WithMessage("Wachtwoord moet minimaal 8 tekens lang zijn.")
+            .Matches(@"[A-Z]").WithMessage("Wachtwoord moet minimaal één hoofdletter bevatten.")
+            .Matches(@"[a-z]").WithMessage("Wachtwoord moet minimaal één kleine letter bevatten.")
+            .Matches(@"[0-9]").WithMessage("Wachtwoord moet minimaal één cijfer bevatten.")
+            .Matches(@"[^\w]").WithMessage("Wachtwoord moet minimaal één speciaal teken bevatten.");
     }
 }
