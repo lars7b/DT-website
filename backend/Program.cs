d1eef4b11c2f878279695c442da1ab12a8f20dce
Lars Zevenbergen <larszevenbergen97@gmail.com>
2026-06-01 13:53:32 +0200

    Redis rate limit (#17)
    Co-authored-by: isharamdhan <159782079+isharamdhan@users.noreply.github.com>
    Co-authored-by: acer <aleksa050806@gmail.com>

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,11 +1,14 @@
+using System.Text;
+using Backend.Middleware;
 using Backend.Repositories;
 using Backend.Services;
 using Backend.Validators;
-using FluentValidation.AspNetCore;
 using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-using System.Text;
+using StackExchange.Redis;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,34 +19,56 @@ builder.Services.AddFluentValidationAutoValidation();
 var jwtSettings = builder.Configuration.GetSection("JwtSettings");
 var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]!);
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
+builder
+    .Services.AddAuthentication(options =>
     {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = jwtSettings["Issuer"],
-        ValidAudience = jwtSettings["Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(key)
-    };
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+        };
+    });
+
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    options.KnownNetworks.Clear();
+    options.KnownProxies.Clear();
 });
 
 // CORS Policy
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowReactApp",
-        policy => policy
-            .WithOrigins("http://localhost:5173")
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-            .AllowCredentials());
+    options.AddPolicy(
+        "AllowReactApp",
+        policy =>
+            policy
+                .WithOrigins("http://localhost:5173")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials()
+    );
+});
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(_ =>
+{
+    var configuration = builder.Configuration;
+
+    string connectionString =
+        configuration.GetConnectionString("RedisDefaultConnection")
+        ?? throw new InvalidOperationException("Redis connection missing");
+
+    return ConnectionMultiplexer.Connect($"{connectionString}");
 });
 
 // dependency injection
@@ -58,7 +83,7 @@ builder.Services.AddScoped<SubcategoryRepository>();
 builder.Services.AddScoped<ProductRepository>();
 builder.Services.AddScoped<FavoriteRepository>();
 builder.Services.AddScoped<OrderHistoryRepository>();
-builder.Services.AddScoped<IPaymentRepository,PaymentRepository>();
+builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
 builder.Services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>();
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 
@@ -71,13 +96,17 @@ builder.Services.AddScoped<IPaymentService, PaymentService>();
 builder.Services.AddScoped<IShoppingCartService, ShoppingCartService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 
+builder.Services.AddTransient<RateLimitMiddleware>();
+
 Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
 
 var app = builder.Build();
 
-app.UseCors("AllowReactApp"); 
+app.UseForwardedHeaders();
+app.UseCors("AllowReactApp");
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseMiddleware<RateLimitMiddleware>();
 app.MapControllers(); // Maps api routes
 
 app.Run();
